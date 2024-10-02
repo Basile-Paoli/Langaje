b0ab8996f79b39116e835eb6f27c8b8515cd0b07
Basile Paoli <basile.paoli@gmail.com>
2024-10-02 19:51:10 +0200

    feat: add basic dynamic arrays implementation

---
+++ structures/dynamic_arrays.c
@@ -0,0 +1,37 @@
+//
+// Created by Basile on 02/10/2024.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "dynamic_arrays.h"
+
+
+dynamicTokenArray *newDynamicTokenArray(int size) {
+    dynamicTokenArray *array = malloc(sizeof(dynamicTokenArray));
+    array->values = malloc(sizeof(token) * size);
+    array->len = 0;
+    array->cap = size;
+
+    return array;
+}
+
+void freeTokenArray(dynamicTokenArray *array) {
+    free(array->values);
+    free(array);
+}
+
+dynamicTokenArray *appendToken(dynamicTokenArray *array, token value) {
+    if (array->len < array->cap) {
+        array->values[array->len++] = value;
+        return array;
+    }
+    array->cap *= 2;
+    token *newValues = realloc(array->values, array->cap * sizeof(token));
+    if (newValues == NULL) {
+        return NULL;
+    }
+    array->values = newValues;
+    array->values[array->len++] = value;
+    return array;
+}
