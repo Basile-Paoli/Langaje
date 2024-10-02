a785b6ae5a11c28ba45f47691f89a1e17300681c
Akas <phelina94@gmail.com>
2024-10-02 22:46:08 +0200

    wip

--- functions.h
+++ functions.h
@@ -1,8 +1,15 @@
 //
-// Created by Lina Phe on 02/10/2024.
+// Define base functions to add, sub, divide, ..
 //
 
 #ifndef LANGAJE_FUNCTIONS_H
 #define LANGAJE_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "types.h"
+
+var add(var *, var *);
 
 #endif //LANGAJE_FUNCTIONS_H
