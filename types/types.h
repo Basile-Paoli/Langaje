5ae2ad60939e6ad87b7b240aa64dd795e8d9ec7b
Natsukira <gazio.mael@gmail.com>
2024-10-02 21:30:58 +0200

    Added types declaration

---
+++ types/types.h
@@ -0,0 +1,40 @@
+//
+// Created by Mael on 02/10/2024.
+//
+
+#ifndef LANGAJE_TYPES_H
+#define LANGAJE_TYPES_H
+
+
+typedef enum varType {
+    _int,
+    _float,
+    _char,
+    _string,
+    _array
+}varType;
+
+typedef struct array{
+    varType type;
+    struct var* values;
+    int capacity;
+    int length;
+}array;
+
+typedef union varValue{
+    int _int;
+    float _float;
+    char _char;
+    char* _string;
+    array* _array;
+}varValue;
+
+
+typedef struct var{
+    varType type;
+    varValue value;
+}var;
+
+
+
+#endif //LANGAJE_TYPES_H
