11d4e7c519c24a2c533fac4def0243e1555a4b02
smi4th <smiath.yt@gmail.com>
2024-10-25 12:23:04 +0200

    mybad, j'ai mal reglé un merge conflict

--- src/main.c
+++ src/main.c
@@ -18,38 +18,6 @@ int main() {
     Lexer *l = new_lexer();
     if (l == NULL)return 1;
 
-    if (l == NULL)return 1;    
-
-    add_lexer_rule(l, new_lexer_rule(l, "def", TOKEN_KEYWORD));
-    add_lexer_rule(l, new_lexer_rule(l, "int", TOKEN_KEYWORD));
-    add_lexer_rule(l, new_lexer_rule(l, "float", TOKEN_KEYWORD));
-
-    add_lexer_rule(l, new_lexer_rule(l, "if", TOKEN_KEYWORD_IF));
-    add_lexer_rule(l, new_lexer_rule(l, "else", TOKEN_KEYWORD_ELSE));
-    add_lexer_rule(l, new_lexer_rule(l, "while", TOKEN_KEYWORD_WHILE));
-    add_lexer_rule(l, new_lexer_rule(l, "for", TOKEN_KEYWORD_FOR));
-
-    add_lexer_rule(l, new_lexer_rule(l, "#LANG_([A-Z]){1,}", TOKEN_PREPROCESSEUR_LANG));
-    add_lexer_rule(l, new_lexer_rule(l, "#include", TOKEN_PREPROCESSEUR_INCLUDE));
-    
-    add_lexer_rule(l, new_lexer_rule(l, "func", TOKEN_FUNCTION_DECLARATION));
-
-    add_lexer_rule(l, new_lexer_rule(l, "==", TOKEN_EQUAL_EQUAL));
-    add_lexer_rule(l, new_lexer_rule(l, "!=", TOKEN_NOT_EQUAL));
-    add_lexer_rule(l, new_lexer_rule(l, "<=", TOKEN_LESS_EQUAL));
-    add_lexer_rule(l, new_lexer_rule(l, ">=", TOKEN_GREATER_EQUAL));
-    add_lexer_rule(l, new_lexer_rule(l, "<", TOKEN_LESS));
-    add_lexer_rule(l, new_lexer_rule(l, ">", TOKEN_GREATER));
-
-    add_lexer_rule(l, new_lexer_rule(l, "&&", TOKEN_AND));
-    add_lexer_rule(l, new_lexer_rule(l, "\\|\\|", TOKEN_OR));
-    add_lexer_rule(l, new_lexer_rule(l, "!|not", TOKEN_NOT));
-
-    add_lexer_rule(l, new_lexer_rule(l, "=", TOKEN_EQUAL));
-
-    add_lexer_rule(l, new_lexer_rule(l, "[0-9]+\\.[0-9]+", TOKEN_FLOAT));
-    add_lexer_rule(l, new_lexer_rule(l, "[0-9]+", TOKEN_NUMBER));
-    add_lexer_rule(l, new_lexer_rule(l, "\"[^\"]*\"", TOKEN_STRING));
     //readLexerFile(l, "CLASSIC.lang");
     readLexerFile(l, "CUSTOM.lang");
 
@@ -59,24 +27,6 @@ int main() {
     add_lexer_rule(l, new_lexer_rule("[0-9]+", TOKEN_INT));
     add_lexer_rule(l, new_lexer_rule("\"[^\"]*\"", TOKEN_STRING));
 
-    add_lexer_rule(l, new_lexer_rule(l, "\\+", TOKEN_ADDITION));
-    add_lexer_rule(l, new_lexer_rule(l, "-", TOKEN_SUBTRACTION));
-    add_lexer_rule(l, new_lexer_rule(l, "\\*", TOKEN_MULTIPLICATION));
-    add_lexer_rule(l, new_lexer_rule(l, "/", TOKEN_DIVISION));
-
-    add_lexer_rule(l, new_lexer_rule(l, "\\(", TOKEN_LPAREN));
-    add_lexer_rule(l, new_lexer_rule(l, "\\)", TOKEN_RPAREN));
-
-    add_lexer_rule(l, new_lexer_rule(l, "\\{", TOKEN_LBRACE));
-    add_lexer_rule(l, new_lexer_rule(l, "\\}", TOKEN_RBRACE));
-
-    add_lexer_rule(l, new_lexer_rule(l, "\\[", TOKEN_LBRACKET));
-    add_lexer_rule(l, new_lexer_rule(l, "\\]", TOKEN_RBRACKET));
-
-    add_lexer_rule(l, new_lexer_rule(l, ",", TOKEN_COMMA));
-    add_lexer_rule(l, new_lexer_rule(l, ";", TOKEN_SEMICOLON));
-
-    add_lexer_rule(l, new_lexer_rule(l, "[a-zA-Z_][a-zA-Z0-9_]{0,}", TOKEN_IDENTIFIER));
     add_lexer_rule(l, new_lexer_rule("[a-zA-Z_][a-zA-Z0-9_]*", TOKEN_IDENTIFIER));
 
     //print_lexer(l);
@@ -98,11 +48,11 @@ int main() {
     printInstructionBlock(pr, 0);
     */
 
-    hmStack* stack = hmStackCreate(BASE_MEMORY_STACK_SIZE);
+    // hmStack* stack = hmStackCreate(BASE_MEMORY_STACK_SIZE);
 
 //    runInstructionBlock(pr,stack);
 
-    hmStackDestroy(stack);
+    // hmStackDestroy(stack);
     free_tokenList(tl);
     free_lexer(l);
     free(input);
